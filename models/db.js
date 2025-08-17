4e637fe55b860ab11a45be19245c7b6b1c334e6f
TheoSudo <theoslaoq@gmail.com>
2025-08-17 17:55:07 -0300

    oi

--- models/db.js
+++ models/db.js
@@ -1,6 +1,19 @@
 import Sequelize from 'sequelize';
-const sequelize = new Sequelize('restaurante', 'root', 'Theo0220!', {
-   host: "localhost",
-   dialect: 'mysql'
-});
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+   process.env.DB_NAME || 'restaurante',
+   process.env.DB_USER || 'root',
+   process.env.DB_PASS || '',
+   {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 3306,
+      dialect: 'mysql',
+      logging: false
+   }
+);
+
 export { Sequelize, sequelize };
+

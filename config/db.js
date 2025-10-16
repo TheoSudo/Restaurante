cef9f284a35829e00ec66e2b932ef9def92615a4
TheoSudo <theoslaoq@gmail.com>
2025-10-16 05:27:23 +0000

    database

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(
+const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
@@ -14,4 +14,4 @@ const sequelize = new Sequelize(
   }
 );
 
-export { Sequelize, sequelize };
+export default db;
